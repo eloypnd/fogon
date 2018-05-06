cfd0778ffd71886d38704045a4771d938c751ff1
Eloy Pineda <code@eloy.email>
2018-05-07 01:47:36 +0200

    add Loading component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Intro } from './Intro'
+export { default as Loading } from './Loading'
 export { default as Timeline } from './Timeline'
 export { default as TimelineItem } from './TimelineItem'
